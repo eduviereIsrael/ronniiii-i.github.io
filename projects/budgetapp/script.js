4907f2b3c6aaa1abef547d48270c6c5987504c60
Roni <ronniiii@yahoo.com>
2022-07-11 18:26:35 +0100

    idk

--- projects/budgetapp/script.js
+++ projects/budgetapp/script.js
@@ -1,3 +1,5 @@
+var page = document.querySelector(".overlay");
+var pageContent = document.querySelector("#content");
 const budgetInput = document.querySelector('#budgetinput');
 const budgetDisplay = document.querySelector('#budgetDisplay');
 const expenseDisplay = document.querySelector('#expenseDisplay');
@@ -5,6 +7,8 @@ const balanceDisplay = document.querySelector('#balanceDisplay');
 const expenseName = document.querySelector('#expenseName');
 const expenseAmount = document.querySelector('#expenseAmount');
 const tbody = document.querySelector('#tbodyy');
+var pageBody = document.getElementById('pageBody');
+const remove = document.querySelector('.js-close');
 
 
 var titles = []
@@ -42,31 +46,12 @@ function displayExpenses() {
         <td>${titles[i]}</td>
         <td>${expenses[i]}</td>
         <td><i class="fas fa-edit" onclick=edit(${i})></i></td>
-        <td><i class="fas fa-minus-circle" onclick=del(${i})></i></td>
+        <td><i class="fas fa-minus-circle" onclick=del(${i})></i></td> 
         </tr>`; 
     }
 
     tbody.innerHTML = htmlCode;
     
-}
-
-function edit(theIndex) {
-    let i = theIndex;
-    expenseName.value = titles[i];
-    expenseAmount.value = expenses[i];
-    type = 'edit';
-    index = i;
-
-    console.log(titles)
-    balance()
-}
-    
-function del(theIndex) {
-    let i = theIndex;
-    titles.splice(i,1);
-    expenses.splice(i,1);
-    displayExpenses()
-    balance()
 }
 function addExpense() {
     var totalexpense = 0
@@ -93,3 +78,51 @@ function addExpense() {
     expenseName.value = " "
     expenseAmount.value = null;
 };
+
+function edit(theIndex) {
+    let i = theIndex;
+    expenseName.value = titles[i];
+    expenseAmount.value = expenses[i];
+    type = 'edit';
+    index = i;
+
+    console.log(titles)
+    balance();
+    addExpense()
+}
+    
+function del(theIndex) {
+    let i = theIndex;
+    titles.splice(i,1);
+    expenses.splice(i,1);
+    displayExpenses()
+    balance()
+    addExpense()
+}
+
+function clearAll() {
+    titles = [];
+    expenses = [];
+    budgetDisplay.innerHTML = 0;
+    expenseDisplay.innerHTML= 0;
+    balanceDisplay.innerHTML = 0;
+
+    displayExpenses();
+    console.log('hed');
+}
+function preview() {
+    page.classList.add('show');
+}
+remove.addEventListener('click', () =>{
+    page.classList.remove('show')
+})
+// function download() {
+//     console.log(courses);
+// }
+function download() {
+    var doc = new jsPDF();  //create jsPDF object
+     doc.fromHTML(pageContent), // page element which you want to print as PDF
+    //  
+     doc.save("courses.pdf"); // save file name as courses.pdf
+    //  console.log('heuu')
+   }
