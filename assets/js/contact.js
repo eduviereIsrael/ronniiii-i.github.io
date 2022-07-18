fe4ea06084c5ef3acf9f38c442617a2972aa7ed8
Roni <ronniiii@yahoo.com>
2022-07-18 12:27:33 +0100

    responsive

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,64 @@
 const tx = document.getElementsByTagName("textarea");
+console.log(tx.length)
 for (let i = 0; i < tx.length; i++) {
-  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].setAttribute("style", "height:" + 50 + "px;overflow-y:auto;");
   tx[i].addEventListener("input", OnInput, false);
 }
+console.log(tx.length)
 
 function OnInput() {
   this.style.height = "auto";
   this.style.height = (this.scrollHeight) + "px";
   console.log('ffd')
+}
+
+const tab = document.querySelectorAll('.tab');
+const tabs = document.querySelectorAll('.tab span');
+const widget = document.querySelectorAll('.inputs .widget');
+const but = document.querySelector('.buttons button');
+const inputs = document.querySelectorAll('[name="details"]');
+
+function next() {
+  inputChange()
+  for (let i = 0; i < widget.length; i++) {
+    tabs[i].innerHTML = inputs[i].value;
+    if (tabs[i].innerHTML !== ""){
+      tab[i].classList.add('show');
+    }
+  }
+  if (widget[2].classList.contains('show')){
+    but.style.display = 'none';
+  }
+}
+function inputChange() {
+  if (widget[0].classList.contains('show')) {
+    if (inputs[0].value == "") {
+      window.alert('fill out fielde');
+    } else {
+      widget[1].classList.add('show');
+      widget[0].classList.remove('show');
+    }
+  }
+  if (widget[1].classList.contains('show')) {
+    if (inputs[1].value !== "") {
+      widget[2].classList.add('show');
+      widget[1].classList.remove('show');
+    } else {
+    }
+  }
+  widget[1].classList.toString()
+}
+for (let i = 0; i < tab.length; i++) {
+  tab[i].addEventListener('click', () => {
+    for (var j=0; j < widget.length; j++) {
+        if(j!==i) widget[j].classList.remove("show");
+    }
+    widget[i].classList.add('show');
+    
+  if (widget[2].classList.contains('show')){
+    but.style.display = 'none';
+  } else{
+    but.style.display = 'inline-block';
+  }
+  })
 }
--- projects/landingpage/script.js
+++ projects/landingpage/script.js
@@ -1,3 +1,10 @@
+const burger = document.querySelector('#burger');
+const burgerLine = document.querySelectorAll('#burger .line');
+const menu = document.querySelector('.navigation .right');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+
 var drop = document.getElementsByClassName('dropdown');
 var link = document.getElementsByClassName('faculties')
 var i 
@@ -16,3 +23,14 @@ for (let i = 0; i < link.length; i++) {
     drop[i].classList.toggle('show');
     });
 }
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    for (let i = 0; i < burgerLine.length; i++) {
+        burgerLine[i].classList.toggle('white');
+    }
+    line2.classList.toggle('disappear');
+    line1.classList.toggle('rot1');
+    line3.classList.toggle('rot2');
+})
+
