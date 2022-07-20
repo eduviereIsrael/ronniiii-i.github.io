1a6f4a57aed4dc6c82f7e8d49f214660d2f00dd2
Roni <ronniiii@yahoo.com>
2022-07-20 20:49:56 +0100

    done

---
+++ projects/intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,26 @@
+const menuBut = document.querySelector('.burger');
+const menu = document.querySelector('.responsive');
+const menuClose = document.querySelector('#close');
+const collTitle = document.querySelectorAll('.collName');
+const dropdown = document.querySelectorAll('.dropdown');
+const arrowIcon = document.querySelectorAll('.responsive a img');
+
+menuBut.addEventListener('click', () => {
+    menu.classList.add('show');
+});
+menuClose.addEventListener('click', () => {
+    menu.classList.remove('show');
+});
+for (let i = 0; i < collTitle.length; i++) {
+    collTitle[i].addEventListener('click', () => {
+        for (var j=0; j < dropdown.length; j++) {
+            if(j!==i) {
+                dropdown[j].classList.remove("show")
+                arrowIcon[j].classList.remove('change');
+            };
+        }
+        dropdown[i].classList.toggle('show');
+        arrowIcon[i].classList.toggle('change');
+        console.log('hi');
+    })
+}
