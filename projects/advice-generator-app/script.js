e66b0a76902e5c1b139a1cd3afe4787b078a4217
Roni <ronniiii@yahoo.com>
2022-07-27 22:05:20 +0100

    advice

--- projects/advice-generator-app/script.js
+++ projects/advice-generator-app/script.js
@@ -3,17 +3,17 @@ const text = document.querySelector('#text');
 const dice = document.querySelector('.green');
 const adviceUrl = "https://api.adviceslip.com/advice"
 
-function display() {
+function display(data) {
     id.innerHTML = data.slip.id;
-    text.innerHTML = data.slip
+    text.innerHTML = data.slip.advice
     console.log(data);
 }
 
 function generateAdvice() {
     fetch(adviceUrl)
     .then(response => response.json())
-    .then(data => display(data))
-    .catch(() => alert('API Could not be reached at this time'))
+    .then((data)=> display(data))// output will be the required data
+    .catch( (error) => console.log(error))
 }
-
+generateAdvice();
 dice.addEventListener('click', generateAdvice);
